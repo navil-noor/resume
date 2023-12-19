154624b9fd11d85e255b8503e6a3d306f9f937c6
Navil <navilnoor@gmail.com>
2023-12-19 15:51:01 +0100

    Drafter Angular Project

---
+++ navil-portfolio/src/app/professional-experience/professional-experience.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professional-experience',
+  templateUrl: './professional-experience.component.html',
+  styleUrl: './professional-experience.component.css'
+})
+export class ProfessionalExperienceComponent {
+
+}
